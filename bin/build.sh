d3bae17c449bc9ccfa37c8db0ee6583f7d6ff688
Phani Raj <phanirajuyn@gmail.com>
2016-02-18 10:30:18 -0500

    Capture start and end times of commands

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,7 @@ set -e
 source_dir="${1:?Source directory is required first arg}"
 output_dir="${2:?Output directory is required second arg}"
 
-(cd "$PWD/$source_dir" && npm install --unsafe-perm)
+(date -u +"%Y-%m-%dT%H:%M:%SZ"  > "$PWD/$source_dir/starttime")
+(cd "$PWD/$source_dir" && npm install --unsafe-perm )
 cp -pr "$PWD/$source_dir/." "$PWD/$output_dir"
+(date -u +"%Y-%m-%dT%H:%M:%SZ"  > "$PWD/$output_dir/endtime")
