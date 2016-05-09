5f15207f2b87e8411234a471300f60be4eaa1123
Phani Raj <PhaniRajuyn@gmail.com>
2016-05-09 13:27:57 -0400

    Removed path handling and use absolute paths passed in from the concourse pipeline (#4)

--- bin/build.sh
+++ bin/build.sh
@@ -9,9 +9,9 @@ echo $source_dir
 echo $output_dir
 
 echo "Pruning git history"
-rm -rf $PWD/$source_dir/.git
+rm -rf $source_dir/.git
 
-(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$PWD/$output_dir/starttime")
-(cd "$PWD/$source_dir" && npm install --unsafe-perm );
-(cp -pr "$PWD/$source_dir/" "$PWD/$output_dir/app");
-(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$PWD/$output_dir/endtime")
+(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$output_dir/starttime")
+(cd "$source_dir" && npm install --unsafe-perm );
+(cp -pr "$source_dir/" "$output_dir/app");
+(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$output_dir/endtime")
--- bin/test.sh
+++ bin/test.sh
@@ -5,7 +5,7 @@ set -e
 source_dir="${1:?Source directory is required first arg}"
 output_dir="${2:?Output directory is required second arg}"
 
-(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$PWD/$output_dir/starttime");
-(cd "$PWD/$source_dir/app" && npm test);
-cp -pr "$PWD/$source_dir/." "$PWD/$output_dir"
-(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$PWD/$output_dir/endtime")
+(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$output_dir/starttime");
+(cd "$source_dir/app" && npm test);
+cp -pr "$source_dir/." "$output_dir"
+(date -u +"%Y-%m-%dT%H:%M:%SZ" > "$output_dir/endtime")
