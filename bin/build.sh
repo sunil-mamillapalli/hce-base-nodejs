663163050c32da62e514276c173eb092f8db793e
Gregory Arcara <greg.arcara@gmail.com>
2016-06-15 09:00:24 -0700

    Fixing exit codes

--- bin/build.sh
+++ bin/build.sh
@@ -6,11 +6,12 @@ source "$(cd $(dirname $BASH_SOURCE)/..; pwd)/lib/build.sh"
 
 action:build() {
   locale-gen en_US.UTF-8
-  
+  local rc=0
   "$HCE_BUILDPACK_LOCATION/bin/compile" \
     "$source_dir" \
-    "$HCE_BUILDPACK_TEMP_DIR"
+    "$HCE_BUILDPACK_TEMP_DIR" || rc=1
   rm -fr "$HCE_BUILDPACK_TEMP_DIR"
+  return $rc
 }
 
 main "$@"
